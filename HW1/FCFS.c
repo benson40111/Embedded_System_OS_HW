839abc4af2e59664822a69aa811746c58e6c1fbd
dirtydirty <benson40111@gmail.com>
2019-11-07 01:12:09 +0800

    update fcfs

--- HW1/FCFS.c
+++ HW1/FCFS.c
@@ -9,89 +9,57 @@
 #include <pthread.h>
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 int cpu_time = 0;
+int total = 0;
 
-static void child_fn(int id, int total)
+void *child_fn(void *num)
 {
+    pthread_mutex_lock(&mutex1);
+    printf("Process (%d)%d was created...\n", num, pthread_self());
+    pthread_mutex_unlock(&mutex1);
+
+    pthread_mutex_lock(&mutex);
     for (int i = 0; i < total; i++) {
         cpu_time++;
-	    printf("Process %d\t%d\n", getpid(), cpu_time);
+	    printf("Process (%d)%d\t%d\n", num, pthread_self(), cpu_time);
     }
-    exit(EXIT_SUCCESS);
+    pthread_mutex_unlock(&mutex);
+
+    pthread_exit(NULL);
+
 }
 
-static void parent_fn(int nproc)
-{
-    int i;
-    for (i = 0; i < nproc; i++)
-	wait(NULL);
-}
-
-static pid_t *pids;
 
 int main(int argc, char *argv[])
 {
-    int ret = EXIT_FAILURE;
-
     if (argc < 3) {
-	fprintf(stderr, "usage: %s <nproc> <total[ms]>\n",
-		argv[0]);
-	exit(EXIT_FAILURE);
+        fprintf(stderr, "usage: %s <nproc> <total[ms]>\n",
+            argv[0]);
+        exit(EXIT_FAILURE);
     }
 
     int nproc = atoi(argv[1]);
-    int total = atoi(argv[2]);
+    total = atoi(argv[2]);
 
     if (nproc < 1) {
-	fprintf(stderr, "<nproc>(%d) should be >= 1\n", nproc);
-	exit(EXIT_FAILURE);
+        fprintf(stderr, "<nproc>(%d) should be >= 1\n", nproc);
+        exit(EXIT_FAILURE);
     }
 
     if (total < 1) {
-	fprintf(stderr, "<total>(%d) should be >= 1\n", total);
-	exit(EXIT_FAILURE);
+        fprintf(stderr, "<total>(%d) should be >= 1\n", total);
+        exit(EXIT_FAILURE);
     }
     
     fflush(stdout);
 
-    pids = malloc(nproc * sizeof(pid_t));
-    if (pids == NULL) {
-	    warn("malloc(pids) failed");
-    }
-
     int ncreated;
-    for (int i = 0, ncreated = 0; i < nproc; i++, ncreated++) {
-        pids[i] = fork();
-        if (pids[i] > 0)
-            printf("Process %d(%d) was created...\n", pids[i], i);
-        
-        pthread_mutex_lock(&mutex);
-        if (pids[i] < 0) {
-            goto wait_children;
-        } else if (pids[i] == 0) {
-            // children
-
-            child_fn(i, total);
-            /* shouldn't reach here */
-        }
-        pthread_mutex_unlock(&mutex);
-    }
-    ret = EXIT_SUCCESS;
-
-    // parent
-
-  wait_children:
-    if (ret == EXIT_FAILURE)
-	for (int i = 0; i < ncreated; i++)
-	    if (kill(pids[i], SIGINT) < 0)
-		warn("kill(%d) failed", pids[i]);
-
-    for (int i = 0; i < ncreated; i++)
-	if (wait(NULL) < 0)
-	    warn("wait() failed.");
-
-  free_pids:
-    free(pids);
-
-    exit(ret);
+    pthread_t t[nproc];
+    for (int i = 0 ; i < nproc ; i++)
+        pthread_create(&t[i], NULL, child_fn, (void *) i);
+    
+    for (int i = 0 ; i < nproc ; i++)
+        pthread_join(t[i], NULL);
+    
 }
