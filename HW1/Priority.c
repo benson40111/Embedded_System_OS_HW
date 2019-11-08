3dd26f66a58a608b671c5e29edab663c67902cd6
dirtydirty <benson40111@gmail.com>
2019-11-08 13:53:42 +0800

    Add barrier

--- HW1/Priority.c
+++ HW1/Priority.c
@@ -7,172 +7,162 @@
 #include <string.h>
 #include <err.h>
 #include <pthread.h>
- 
+
 #define NLOOP_FOR_ESTIMATION 1000000000UL
 #define NSECS_PER_MSEC 1000000UL
 #define NSECS_PER_SEC 1000000000UL
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_barrier_t barrier;
 
-static inline long diff_nsec(struct timespec before, struct timespec after)
+static inline long diff_usec(struct timespec before, struct timespec after)
 {
-        return ((after.tv_sec * NSECS_PER_SEC + after.tv_nsec)
-                - (before.tv_sec * NSECS_PER_SEC + before.tv_nsec));
-	
+	return ((after.tv_sec * NSECS_PER_SEC + after.tv_nsec)
+			- (before.tv_sec * NSECS_PER_SEC + before.tv_nsec));
 }
 
 static unsigned long loops_per_msec()
 {
-        struct timespec before, after;
-        clock_gettime(CLOCK_MONOTONIC, &before);
+	unsigned long i;
+	struct timespec before, after;
 
-        unsigned long i;
-        for (i = 0; i < NLOOP_FOR_ESTIMATION; i++)
+	clock_gettime(CLOCK_MONOTONIC, &before);
+
+	for (i = 0; i < NLOOP_FOR_ESTIMATION; i++)
 		;
 
-        clock_gettime(CLOCK_MONOTONIC, &after);
+	clock_gettime(CLOCK_MONOTONIC, &after);
 
 	int ret;
-        return  NLOOP_FOR_ESTIMATION * NSECS_PER_MSEC / diff_nsec(before, after);
+	return  NLOOP_FOR_ESTIMATION * NSECS_PER_MSEC / diff_usec(before, after);
 }
- 
+
 static inline void load(unsigned long nloop)
 {
-        unsigned long i;
-        for (i = 0; i < nloop; i++)
-                ;
+	unsigned long i;
+	for (i = 0; i < nloop; i++)
+		;
 }
 
 static void child_fn(int id, struct timespec *buf, int nrecord, unsigned long nloop_per_resol, struct timespec start)
 {
-        printf("mutex lock %d\n", pthread_mutex_lock(&mutex));
-        int i;
-        for (i = 0; i < nrecord; i++) {
-                struct timespec ts;
+        pthread_barrier_wait(&barrier);
+	int i;
+	for (i = 0; i < nrecord; i++) {
+		struct timespec ts;
 
-                load(nloop_per_resol);
-                clock_gettime(CLOCK_MONOTONIC, &ts);
-                buf[i] = ts;
-        }
-        for (i = 0; i < nrecord; i++) {
-                printf("%d\t%ld\t%d\n", id, diff_nsec(start, buf[i]) / NSECS_PER_MSEC, (i + 1) * 100 / nrecord);
-        }
+		load(nloop_per_resol);
+		clock_gettime(CLOCK_MONOTONIC, &ts);
+		buf[i] = ts;
+	}
+                printf("Process %d is running...\n", getpid());
 
-        exit(EXIT_SUCCESS);
-        printf("mutex unlock %d\n", pthread_mutex_unlock(&mutex));
-
+	for (i = 0; i < nrecord; i++) {
+		printf("%d\t%ld\t%d\n", id, diff_usec(start, buf[i]) / NSECS_PER_MSEC, (i + 1) * 100 / nrecord);
+	}
+	exit(EXIT_SUCCESS);
 }
- 
+
 static void parent_fn(int nproc)
 {
-        int i;
-        for (i = 0; i < nproc; i++)
-                wait(NULL);
+	int i;
+	for (i = 0; i < nproc; i++)
+		wait(NULL);
 }
 
 static pid_t *pids;
 
 int main(int argc, char *argv[])
 {
-        int ret = EXIT_FAILURE;
+	int ret = EXIT_FAILURE;
 
-        if (argc < 3) {
-                fprintf(stderr, "usage: %s <nproc> <total[ms]> <resolution[ms]>\n", argv[0]);
-                exit(EXIT_FAILURE);
-        }
+	if (argc < 3) {
+		fprintf(stderr, "usage: %s <total[ms]> <resolution[ms]>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
-        int nproc = 3;
-        int total = atoi(argv[1]);
-        int resol = atoi(argv[2]);
+	int nproc = 3;
+	int total = atoi(argv[1]);
+	int resol = atoi(argv[2]);
 
-        if (nproc < 1) {
-                fprintf(stderr, "<nproc>(%d) should be >= 1\n", nproc);
-                exit(EXIT_FAILURE);
-        }
+	if (total < 1) {
+		fprintf(stderr, "<total>(%d) should be >= 1\n", total);
+		exit(EXIT_FAILURE);
+	}
 
-        if (total < 1) {
-                fprintf(stderr, "<total>(%d) should be >= 1\n", total);
-                exit(EXIT_FAILURE);
-        }
+	if (resol < 1) {
+		fprintf(stderr, "<resol>(%d) should be >= １\n", resol);
+		exit(EXIT_FAILURE);
+	}
 
-        if (resol < 1) {
-                fprintf(stderr, "<resol>(%d) should be >= ��\n", resol);
-                exit(EXIT_FAILURE);
-        }
+	if (total % resol) {
+		fprintf(stderr, "<total>(%d) should be multiple of <resolution>(%d)\n", total, resol);
+		exit(EXIT_FAILURE);
+	}
+	int nrecord = total / resol;
 
-        if (total % resol) {
-                fprintf(stderr, "<total>(%d) should be multiple of <resolution>(%d)\n", total, resol);
-                exit(EXIT_FAILURE);
-        }
-        int nrecord = total / resol;
-
-        struct timespec *logbuf = malloc(nrecord * sizeof(struct timespec));
+	struct timespec *logbuf = malloc(nrecord * sizeof(struct timespec));
 	if (!logbuf)
 		err(EXIT_FAILURE, "malloc(logbuf) failed");
 
-	puts("estimating workload which takes just one milisecond");
-        unsigned long nloop_per_resol = loops_per_msec() * resol;
-	puts("end estimation");
-	fflush(stdout);
+	unsigned long nloop_per_resol = loops_per_msec() * resol;
 
-        pids = malloc(nproc * sizeof(pid_t));
-        if (pids == NULL) {
-                warn("malloc(pids) failed");
-                goto free_logbuf;
-        }
+	pids = malloc(nproc * sizeof(pid_t));
+	if (pids == NULL) {
+		warn("malloc(pids) failed");
+		goto free_logbuf;
+	}
 
-        struct timespec start;
-        clock_gettime(CLOCK_MONOTONIC, &start);
+	struct timespec start;
+	clock_gettime(CLOCK_MONOTONIC, &start);
 
-        int i, ncreated;
-        for (i = 0, ncreated = 0; i < nproc; i++, ncreated++) {
-                pids[i] = fork();
-                if (pids[i] < 0) {
-                        goto wait_children;
-                } else if (pids[i] == 0) {
-                        // children
-                        switch (i)
-                        {
-                        case 0:
-                                printf("%d\n",nice(0));
-                                break;
-                        case 1:
-                                printf("%d\n",nice(10));
-                                break;
-                        case 2:
-                                printf("%d\n",nice(5));
-                                break;
-                        
-                        default:
-                                printf("Error!\n");
-                                break;
-                        }
-                            
-                        child_fn(i, logbuf, nrecord, nloop_per_resol, start);
-                        
-                        /* shouldn't reach here */
-                }
-        }
-        ret = EXIT_SUCCESS;
+	int i, ncreated;
+	for (i = 0, ncreated = 0; i < nproc; i++, ncreated++) {
+		pids[i] = fork();
+		if (pids[i] < 0) {
+			goto wait_children;
+		} else if (pids[i] == 0) {
+			// children
 
-        // parent
+			if (i == 0) {
+				setpriority(PRIO_PROCESS, getpid(), 5);;
+				printf("Process(%d) priority:%d\n",  getpid(), getpriority(PRIO_PROCESS,getpid()));
+                                child_fn(i, logbuf, nrecord, nloop_per_resol, start);
+
+			}
+			else if (i == 1) {
+				setpriority(PRIO_PROCESS, getpid(), 10);;
+				printf("Process(%d) priority:%d\n", getpid(), getpriority(PRIO_PROCESS,getpid()));
+                                child_fn(i, logbuf, nrecord, nloop_per_resol, start);
+
+			}
+			else {
+				setpriority(PRIO_PROCESS, getpid(), -5);;
+				printf("Process(%d) priority:%d\n", getpid(), getpriority(PRIO_PROCESS,getpid()));
+                                child_fn(i, logbuf, nrecord, nloop_per_resol, start);
+
+			}
+			/* shouldn't reach here */
+		}
+	}
+	ret = EXIT_SUCCESS;
+
+	// parent
 
 wait_children:
-        if (ret == EXIT_FAILURE)
-                for (i = 0; i < ncreated; i++)
-                        if (kill(pids[i], SIGINT) < 0)
-                                warn("kill(%d) failed", pids[i]);
+	if (ret == EXIT_FAILURE)
+		for (i = 0; i < ncreated; i++)
+			if (kill(pids[i], SIGINT) < 0)
+				warn("kill(%d) failed", pids[i]);
 
-
-        for (i = 0; i < ncreated; i++)
-                if (wait(NULL) < 0)
-                        warn("wait() failed.");
+	for (i = 0; i < ncreated; i++)
+		if (wait(NULL) < 0)
+			warn("wait() failed.");
 
 free_pids:
-        free(pids);
+	free(pids);
 
 free_logbuf:
-        free(logbuf);
+	free(logbuf);
 
-        exit(ret);
+	exit(ret);
 }
