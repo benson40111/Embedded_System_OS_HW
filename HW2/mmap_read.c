37bcc8be28e855191808cc29ed2900ac2a72fc37
benson40111 <benson40111@gmail.com>
2019-11-11 22:46:15 +0800

    Complete mmap

--- HW2/mmap_read.c
+++ HW2/mmap_read.c
@@ -15,32 +15,37 @@ int main()
     struct timeval tv1, tv2;
 	char s[] = "This is hello";
     
+	printf("Standard I/O...\n");
     //read
 
     gettimeofday(&tv1, NULL);
-    fd = open("testfile", O_WRONLY);
-
+    fd = open("testfile_write", O_WRONLY);
 	write(fd, s, sizeof(s));
-
     close(fd);
-	fd = open("testfile", O_RDONLY);
+	fd = open("testfile_write", O_RDONLY);
 	size = read(fd, buffer, sizeof(buffer));
 	close(fd);
     gettimeofday(&tv2, NULL);
 
+	printf("Read text is:%s\n", buffer);
     printf("Time of read/write: %dms\n", tv2.tv_usec - tv1.tv_usec);
-	printf("Write text is:%s\n", buffer);
     
     //mmap
+	printf("mmap...\n");
 
+	char buffer1[100];
+	size = 10;
     gettimeofday(&tv1, NULL);
-    fd = open("testfile", O_RDWR);
-	size = read(fd, buffer, sizeof(buffer));
-	char *map = mmap(NULL, size, PROT_READ, MAP_SHARED, fd, 0);
-	munmap(map, size);
+    fd = open("testfile_mmap", O_RDWR);
+	char *map = (char*)malloc(size * sizeof(char));
+	map = (char*)mmap(NULL, sizeof(char)*size, PROT_WRITE, MAP_SHARED, fd, 0);
+	map = s;
+	munmap(map, sizeof(char)*size);
 	close(fd);
     gettimeofday(&tv2, NULL);
-	printf("Read text is:%s\n", buffer);
+
+	printf("Read text is:%s\n", map);
     printf("Time of mmap: %dms\n", tv2.tv_usec - tv1.tv_usec);
+	memset(map, 0, size * sizeof(char));
     return 0;
 }
