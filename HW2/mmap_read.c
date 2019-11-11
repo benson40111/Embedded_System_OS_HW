cb9a0be06c38385dedda1f78e82dea1a298699c5
benson40111 <benson40111@gmail.com>
2019-11-11 16:11:33 +0800

    Add open read write

--- HW2/mmap_read.c
+++ HW2/mmap_read.c
@@ -8,54 +8,34 @@
 #include<fcntl.h>
 #include<sys/mman.h>
 
-#define MAX 10000
-
 int main()
 {
-    int i = 0;
-    int count = 0, fd = 0;
+    int size = 0, fd = 0;
+	char buffer[100];
     struct timeval tv1, tv2;
-    int *array = (int *)malloc(sizeof(int)*MAX);
+	char *s = "This is hello\n";
     
     //read
 
     gettimeofday(&tv1, NULL);
-    fd = open("mmap test", O_RDWR);
-    if (sizeof(int)*MAX != read(fd, (void *)array, sizeof(int)*MAX)))
-    {
-        pruntf("Reading data failed...\n");
-        return -1;
-    }
+    fd = open("testfile", O_WRONLY|O_CREAT);
 
-    for (i = 0 ; i < MAX ; i++)
-        array[i]++;
+	write(fd, s, sizeof(s));
 
-    if (sizeof(int)*MAX != write(fd, (void *)array, sizeof(int)*MAX))
-    {
-        printf("Writing data failed...\n");
-        return -1;
-    }
-
-    free(array);
     close(fd);
+	fd = open("testfile", O_RDONLY);
+	size = read(fd, buffer, sizeof(buffer));
+	close(fd);
     gettimeofday(&tv2, NULL);
 
-    printf("Time of read/write: %dms/n", tv2.tv_usec - tv1.tv_usec);
+    printf("Time of read/write: %dms\n", tv2.tv_usec - tv1.tv_usec);
+	printf("Write text is:%s\n", buffer);
     
     //mmap
 
     gettimeofday(&tv1, NULL);
     fd = open("mmap test", O_RDWR);
-    array = mmap(NULL, sizeof(int)*MAX, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
-
-    for (i = 0 ; i < MAX ; i++)
-        array[i]++;
-    
-    munmap(array, sizeof(int)*MAX);
-    msync(array, sizeof(int)*MAX, MS_SYNC);
-    free(array);
-    close(fd);
     gettimeofday(&tv2, NULL);
-    printf("Time of mmap: %dms/n", tv2.tv_usec - tv1.tv_usec);
+    printf("Time of mmap: %dms\n", tv2.tv_usec - tv1.tv_usec);
     return 0;
 }
